d3a936176113dfb8a1eace643736a450032dd850
FabienC <fabiencoutant.ctt@gmail.com>
2024-06-12 17:44:11 +0300

    fix: contract version

--- contracts/interfaces/IERC20MintableAndBurnable.sol
+++ contracts/interfaces/IERC20MintableAndBurnable.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.20;
+pragma solidity 0.8.22;
 
 interface IERC20MintableAndBurnable {
     function mint(address account, uint256 amount) external;
