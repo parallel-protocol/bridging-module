df898e2330186c0ec9bf3cf22f648c408d1138f9
Hashanon <hashanon@protonmail.com>
2024-06-21 17:40:02 +0300

    refactor: switch case return

--- utils/getRpcURL.ts
+++ utils/getRpcURL.ts
@@ -1,23 +1,19 @@
 export const getRpcURL = (network: string): string => {
     const apiKey = process.env.ALCHEMY_API_KEY
     if (!apiKey) throw new Error('ALCHEMY_API_KEY is not set')
-    let baseURL = ''
+
     switch (network) {
         case 'sepolia': {
-            baseURL = 'https://eth-sepolia.g.alchemy.com/v2/'
-            break
+            return `https://eth-sepolia.g.alchemy.com/v2/${apiKey}`
         }
         case 'amoy': {
-            baseURL = 'https://polygon-amoy.g.alchemy.com/v2/'
-            break
+            return `https://polygon-amoy.g.alchemy.com/v2/${apiKey}`
         }
         case 'arbSepolia': {
-            baseURL = 'https://arb-sepolia.g.alchemy.com/v2/'
-            break
+            return `https://arb-sepolia.g.alchemy.com/v2/${apiKey}`
         }
         default: {
             throw new Error(`${network} Network RPC not configured`)
         }
     }
-    return `${baseURL}${apiKey}`
 }
--- utils/getVerifyConfig.ts
+++ utils/getVerifyConfig.ts
@@ -1,37 +1,41 @@
 type VerifyConfig = {
     etherscan: {
         apiUrl: string
-        apiKey: string | undefined
+        apiKey: string
     }
 }
 
 export const getVerifyConfig = (network: string): VerifyConfig => {
-    const config: VerifyConfig = { etherscan: { apiUrl: '', apiKey: undefined } }
     switch (network) {
         case 'sepolia': {
-            config.etherscan = {
-                apiUrl: 'https://api-sepolia.etherscan.io/',
-                apiKey: process.env.ETHERSCAN_API_KEY,
+            if (!process.env.ETHERSCAN_API_KEY) throw new Error('ETHERSCAN_API_KEY is not set')
+            return {
+                etherscan: {
+                    apiUrl: 'https://api-sepolia.etherscan.io',
+                    apiKey: process.env.ETHERSCAN_API_KEY,
+                },
             }
-            break
         }
         case 'amoy': {
-            config.etherscan = {
-                apiUrl: 'https://api-amoy.polygonscan.com/',
-                apiKey: process.env.POLYSCAN_API_KEY,
+            if (!process.env.POLYSCAN_API_KEY) throw new Error('POLYSCAN_API_KEY is not set')
+            return {
+                etherscan: {
+                    apiUrl: 'https://api-amoy.polygonscan.com',
+                    apiKey: process.env.POLYSCAN_API_KEY,
+                },
             }
-            break
         }
         case 'arbSepolia': {
-            config.etherscan = {
-                apiUrl: 'https://api-sepolia.arbiscan.io/',
-                apiKey: process.env.ARBISCAN_API_KEY,
+            if (!process.env.ARBISCAN_API_KEY) throw new Error('ARBISCAN_API_KEY is not set')
+            return {
+                etherscan: {
+                    apiUrl: 'https://api-sepolia.arbiscan.io',
+                    apiKey: process.env.ARBISCAN_API_KEY,
+                },
             }
-            break
         }
         default: {
             throw new Error(`${network} Network Verify not configured`)
         }
     }
-    return config
 }
